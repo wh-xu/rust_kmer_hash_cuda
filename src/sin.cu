b3cfda6e5f83944046f9e0245d51334c58f1a7de
wh-xu <wexu@ucsd.edu>
2024-04-27 22:21:51 +0800

    Init.

---
+++ src/sin.cu
@@ -0,0 +1,7 @@
+
+extern "C" __global__ void sin_kernel(float *out, const float *inp, int numel) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < numel) {
+        out[i] = sin(inp[i]);
+    }
+}
